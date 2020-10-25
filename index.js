a085ce64ce4dc948d03f4404c6249dac30b00925
tavolafourcade <tavolafourcade@gmail.com>
2020-10-25 11:49:08 -0500

    Add animation to the nav side in order to fade in and fade out

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ let nav = get("nav");
 let exit = get("exit-btn");
 
 
-//Targeting when somebody open the hamburger button
+//Targeting when somebody open the hamburger SVG
 open.addEventListener('click', () =>{
   nav.classList.add('open-nav');
 })
 
-
+//Targeting when somebody click the close SVG
 exit.addEventListener('click', () =>{
   nav.classList.remove('open-nav');
 })
